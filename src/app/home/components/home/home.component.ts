398f1434d24347b5b0f743185ff5b559d435e2bf
kirandash <thekirankumardash@gmail.com>
2020-10-19 01:50:40 +0800

    Styling home page with flex and add Router Navigation

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public goToRSSFeeds(): void {
+    this.router.navigate(['rssfeeds']);
   }
-
 }
