6b202562d292b9b4c68d44665c39f53b4c9fad3b
kirandash <thekirankumardash@gmail.com>
2020-10-19 00:47:03 +0800

    Intial setup of the application with basic routing and Home page

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from 'src/app/home/components/home/home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule],
+})
+export class HomeModule {}
